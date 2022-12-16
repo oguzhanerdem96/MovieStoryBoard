a68d57b42a334e57fc4c923770992d03a79f6871
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2022-12-16 23:30:19 +0300

    storyboard building

---
+++ MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC.swift
+//  MovieStoryBoard
+//
+//  Created by Oğuzhan Erdem on 16.12.2022.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
