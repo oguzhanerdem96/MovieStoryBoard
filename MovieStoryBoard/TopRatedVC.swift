de0a83e122dd30eb7385722dffa1a1c889c0a1dc
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2022-12-19 23:28:04 +0300

    Storyboard design continue

---
+++ MovieStoryBoard/TopRatedVC.swift
@@ -0,0 +1,29 @@
+//
+//  TopRatedVC.swift
+//  MovieStoryBoard
+//
+//  Created by Oğuzhan Erdem on 19.12.2022.
+//
+
+import UIKit
+
+class TopRatedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
