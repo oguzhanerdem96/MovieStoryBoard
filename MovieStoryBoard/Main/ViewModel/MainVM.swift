be3eb7bf13b463234f7c53577e4b5f8d17297819
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2022-12-24 23:48:17 +0300

    ViewModel creating

--- MovieStoryBoard/Main/ViewModel/MainVM.swift
+++ MovieStoryBoard/Main/ViewModel/MainVM.swift
@@ -7,4 +7,25 @@
 
 import Foundation
 
+protocol MainVMDelegate: AnyObject {
+    func didGetTopRated(isDone: Bool )
+}
 
+class MainVM {
+    weak var delegate: MainVMDelegate?
+    static let shared = MainVM()
+    private init(){}
+    
+    var topRated: [ResultMovie] = []
+    private var page: Int = 1
+    
+    func getTopRated(completion: @escaping ((String?)->())) {
+        TopRatedManager.shared.getTopRated(page: page) { items, errorMessage in
+            if let items = items {
+                self.topRated.append(contentsOf: items.results!)
+                self.page += 1
+                self.delegate?.didGetTopRated(isDone: false)
+            }
+        }
+    }
+}
