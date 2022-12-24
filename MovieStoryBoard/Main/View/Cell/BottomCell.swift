a71e14133427b0bf9b5b85aed3a2199ade0a15f5
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2022-12-24 23:41:24 +0300

    configure cells

---
+++ MovieStoryBoard/Main/View/Cell/BottomCell.swift
@@ -0,0 +1,32 @@
+//
+//  BottomCell.swift
+//  MovieStoryBoard
+//
+//  Created by Oğuzhan Erdem on 24.12.2022.
+//
+
+import UIKit
+
+class BottomCell: UICollectionViewCell {
+    @IBOutlet weak var bottomCellView: UIView!
+    @IBOutlet weak var bottomCellImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        bottomCellView.layer.cornerRadius = 10
+        bottomCellView.layer.shadowColor = UIColor.darkGray.cgColor
+        bottomCellView.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        bottomCellView.layer.shadowRadius = 4.0
+        bottomCellView.layer.shadowOpacity = 0.4
+        
+        bottomCellImageView.layer.cornerRadius = 10
+        bottomCellImageView.clipsToBounds = true
+        
+    }
+    func configureCell(item: ResultMovie) {
+        let image = NetworkHelper.shared.baseImageUrl + (item.poster_path ?? "")
+        self.bottomCellImageView.kf.indicatorType = .activity
+        self.bottomCellImageView.kf.setImage(with: URL(string: image))
+    }
+
+}
