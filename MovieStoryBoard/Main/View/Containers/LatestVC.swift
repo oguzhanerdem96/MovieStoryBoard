1dcc595573292042b24a206fd9490eba19bff4de
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2022-12-29 21:06:36 +0300

    Popular,Latest,TopRated configure

--- MovieStoryBoard/Main/View/Containers/LatestVC.swift
+++ MovieStoryBoard/Main/View/Containers/LatestVC.swift
@@ -11,14 +11,76 @@ class LatestVC: UIViewController {
     @IBOutlet weak var latestCollectionView: UICollectionView!
     
     
-
+    let bottomCell = "BottomCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setupUI()
     }
     
+    private func setupUI() {
+        latestCollectionView.delegate = self
+        latestCollectionView.dataSource = self
+        latestCollectionView.register(.init(nibName: bottomCell, bundle: nil), forCellWithReuseIdentifier: bottomCell)
+        LatestVM.shared.delegate = self
+        LatestVM.shared.getLatest{ errorMessage in
+            if let errorMessage = errorMessage {
+                print("error \(errorMessage)")
+            }
+        }
+    }
+}
 
+extension LatestVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return LatestVM.shared.latest.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: bottomCell, for: indexPath) as! BottomCell
+        let item = LatestVM.shared.latest[indexPath.row]
+        cell.configureCell(item: item)
+        cell.backgroundColor = UIColor.clear
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        DetailVM.shared.movieID = LatestVM.shared.latest[indexPath.row].id
+        AboutVC.detailAbout = LatestVM.shared.latest[indexPath.row].overview
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (collectionView.frame.width + 60 ) / 4 , height: (collectionView.frame.height + 40) / 2.5)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY >= contentHeight - (4 * height) {
+            
+            LatestVM.shared.getLatest{ errorMessage in
+                if let errorMessage = errorMessage {
+                    print("error \(errorMessage)")
+                }
+            }
+            DispatchQueue.main.async {
+                self.latestCollectionView.reloadData()
+            }
+        }
+    }
+}
 
+extension LatestVC: LatestDelegate {
+    func didGetLatest(isDone: Bool) {
+        if isDone {
+            DispatchQueue.main.async {
+                self.latestCollectionView.reloadData()
+            }
+        }
+    }
 }
--- MovieStoryBoard/Main/View/Containers/TopRatedVC.swift
+++ MovieStoryBoard/Main/View/Containers/TopRatedVC.swift
@@ -11,11 +11,76 @@ class TopRatedVC: UIViewController {
 
     @IBOutlet weak var topRatedCollectionView: UICollectionView!
     
+    let bottomCell = "BottomCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        setupUI()
     }
     
+    private func setupUI() {
+        topRatedCollectionView.delegate = self
+        topRatedCollectionView.dataSource = self
+        topRatedCollectionView.register(.init(nibName: bottomCell, bundle: nil), forCellWithReuseIdentifier: bottomCell)
+        TopRatedVM.shared.delegate = self
+        TopRatedVM.shared.getTopRated{ errorMessage in
+            if let errorMessage = errorMessage {
+                print("error \(errorMessage)")
+            }
+        }
+    }
+}
 
+extension TopRatedVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return TopRatedVM.shared.topRated.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: bottomCell, for: indexPath) as! BottomCell
+        let item = TopRatedVM.shared.topRated[indexPath.row]
+        cell.configureCell(item: item)
+        cell.backgroundColor = UIColor.clear
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        DetailVM.shared.movieID = TopRatedVM.shared.topRated[indexPath.row].id
+        AboutVC.detailAbout = TopRatedVM.shared.topRated[indexPath.row].overview
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (collectionView.frame.width + 60 ) / 4 , height: (collectionView.frame.height + 40) / 2.5)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY >= contentHeight - (4 * height) {
+            
+            TopRatedVM.shared.getTopRated{ errorMessage in
+                if let errorMessage = errorMessage {
+                    print("error \(errorMessage)")
+                }
+            }
+            DispatchQueue.main.async {
+                self.topRatedCollectionView.reloadData()
+            }
+        }
+    }
+}
+
+extension TopRatedVC: TopRatedDelegate {
+    func didGetTopRated(isDone: Bool) {
+        if isDone {
+            DispatchQueue.main.async {
+                self.topRatedCollectionView.reloadData()
+            }
+        }
+    }
 }
